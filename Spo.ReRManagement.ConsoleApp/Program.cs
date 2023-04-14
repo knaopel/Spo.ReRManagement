0dbd7378efa11e8966cfb4db212d8914151e18dc
Kurt Opel <Kurt.Opel@plantemoran.com>
2023-04-14 11:17:04 -0400

    Add project files.

---
+++ Spo.ReRManagement.ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using CommandLine;
+using Microsoft.Extensions.Configuration;
+
+namespace Spo.ReRManagement.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args).WithParsed(o => { Console.WriteLine($"ClientId: {o.ClientId}."); });
+            IConfiguration config = new ConfigurationBuilder().AddJsonFile("appsettings.json").AddEnvironmentVariables().Build();
+        }
+    }
+}
